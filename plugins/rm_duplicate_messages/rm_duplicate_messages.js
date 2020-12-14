3032f95d524aa91ab11c53c5cdf265d92a7942fc
kripton20 <kripton20@mail.ru>
2020-12-14 18:00:56 +0300

    Версия 20 Дата: 14,12.2020 Время: 18.00

--- plugins/rm_duplicate_messages/rm_duplicate_messages.js
+++ plugins/rm_duplicate_messages/rm_duplicate_messages.js
@@ -1,65 +1,72 @@
-/**
-** Клиентские скрипты и элементы пользовательского интерфейса
-* Клиентский сценарий может фактически использовать все методы объекта приложения Roundcube.
-* Этот объект приложения, к которому можно получить доступ, window.rcmail также предоставляет перехватчики, но немного другим способом:
-*  подобно DOM браузера, можно регистрировать прослушиватели событий, используя следующие методы:
-*     rcmail.addEventListener('event', callback);
-*     rcmail.removeEventListener('event', callback);
-*
-* Самое главное событие конечно есть init. Здесь плагин может добавлять кнопки и регистрировать свои собственные команды в основном сценарии приложения.
-* Вот пример, показывающий, как может выглядеть скрипт плагина:
-*    rcmail.addEventListener('init', function(evt) {
-*       // создаем пользовательскую кнопку
-*       var button = $('<A>').attr('id', 'rcmSampleButton').html(rcmail.gettext('buttontitle', 'sampleplugin'));
-*       button.bind('click', function(e){ return rcmail.command('plugin.samplecmd', this); });
-*       // добавляем кнопку в контейнер панели инструментов и регистрируем
-*       rcmail.add_element(button, 'toolbar');
-*       // сообщим приложению, где находится кнопка и за какую команду она отвечает
-*       rcmail.register_button('plugin.samplecmd', 'rcmSampleButton', 'link');
-*       // свяжем пользовательскую команду (запускаемую обработчиком onclick кнопки) с функцией обратного вызова скрипта клиента плагина
-*       rcmail.register_command('plugin.samplecmd', sample_handler, true);
-*     });
-* // Документация по JavaScript API здесь: https://github.com/roundcube/roundcubemail/wiki/Javascript-API
-// https://github.com/roundcube/roundcubemail/wiki/Javascript-API
-*/
-function button_function () {
-	//
+function msg_search(day1, month1, year1, day2, month2, year2){
+	// делаем запрос на сервер - берём массив списка писем
+	
+	// включаем блокировку интерфейса, выводим сообщение о работе процедуры поиска дубликатов
+	var lock = rcmail.set_busy(true, 'rm_duplicate_messages.checkdpl'),
+	// этот параметр для того чтобы это сообщение перекрывалось следующим сообщением
+	// о том что процедура поиска дубликатов сообщений завершена
+	params = rcmail.check_recent_params();
+	// запускаем PHP-функцию поиска дубликатов сообщений - rm_dublecates
+	// вызываем метод http_post объекта rcmail (параметры через запятую)
+	rcmail.http_post('plugin.rm_dublecates', params, lock);
+}
+// зупускаем процедуру поиска/удаления дубликатов сообщений
+function msg_search_start () {
+	// переменные диалогового окна
+	//var day1;
+	// контент диалогового окна
+	var content = '<H3 align="center">'+rcmail.get_label('rm_duplicate_messages.label4')+'</H4><br/>'+
+	// формируем таблицу выбора даты
+	// аттрибут cellspacing - расстояние между ячейками
+	'<table border="0" align="center" cellpadding="0" cellspacing="10">'+
+	'<tr align="center"><td></td>'+
+	// получаем надписи на локализованном языке из файла локализации
+	'<td><strong>'+rcmail.get_label('rm_duplicate_messages.label5')+'</strong></td>'+
+	'<td><strong>'+rcmail.get_label('rm_duplicate_messages.label6')+'</strong></td>'+
+	'<td><strong>'+rcmail.get_label('rm_duplicate_messages.label7')+'</strong></td></tr>'+
+	'<tr align="center"><td><strong>'+rcmail.get_label('rm_duplicate_messages.label8')+'</strong></td>'+
+	'<td><input type="number" id="day1" size="5" min="1" max="31" value="1" tabindex="0" style="padding-bottom:5px; padding-top:5px" /></td>'+
+	'<td><input type="number" id="month1" size="5" min="1" max="12" value="1" tabindex="0" style="padding-bottom:5px; padding-top:5px" /></td>'+
+	'<td><input type="number" id="year1" size="5" min="1990" max="2050" value="1996" tabindex="0" style="padding-bottom:5px; padding-top:5px" /></td></tr>'+
+	'<tr align="center"><td><strong>'+rcmail.get_label('rm_duplicate_messages.label9')+'</strong></td>'+
+	'<td><input type="number" id="day2" size="5" min="1" max="31" value="1" tabindex="0" style="padding-bottom:5px; padding-top:5px" /></td>'+
+	'<td><input type="number" id="month2" size="5" min="1" max="12" value="1" tabindex="0" style="padding-bottom:5px; padding-top:5px" /></td>'+
+	'<td><input type="number" id="year2" size="5" min="1990" max="2050" value="1996" tabindex="0" style="padding-bottom:5px; padding-top:5px" /></td>'+
+	'</tr></table>';
+	// заголовок диалогового окна
+	var title = rcmail.get_label('rm_duplicate_messages.label3');
+	// кнопки диалогового окна
 	buttons={
 	};
-	// кнопка запуска процедуры проверки
-	buttons[rcmail.get_label('rm_duplicate_messages.label5')] = function(e) {
-		// включаем блокировку интерфейса, выводим сообщение о работе процедуры поиска дубликатов
-		///var lock = rcmail.set_busy(true, 'rm_duplicate_messages.checkdpl'),
-		// этот параметр для того чтобы это сообщение перекрывалось следующим сообщением
-		// о том что процедура поиска дубликатов сообщений завершена
-		///params = rcmail.check_recent_params();
+	// кнопка запуска процедуры - 'Отметить дубликаты'
+	buttons[rcmail.get_label('rm_duplicate_messages.label10')] = function(e) {
+		// получаем значения полей формы
+		var day1 = document.getElementById('day1').value,
+		month1 = document.getElementById('month1').value,
+		year1 = document.getElementById('year1').value,
+		day2 = document.getElementById('day2').value,
+		month2 = document.getElementById('month2').value,
+		year2 = document.getElementById('year2').value;
 		// отключим нашу коммандную кнопку
 		window.rcmail.enable_command('plugin.btn_cmd_rm_dublecates', false);
-		// запускаем PHP-функцию поиска дубликатов сообщений - rm_dublecates
-		// вызываем метод http_post объекта rcmail (параметры через запятую)
-		///rcmail.http_post('plugin.rm_dublecates', params, lock);
-
-		/*//		Повторная обработка данных ответа на клиенте:
-		//    function some_callback_function(response)
-		//    {
-		//      $('#somecontainer').html(response.message);
-		//    }
-		//Обратите внимание, что функция обратного вызова может принимать только один аргу-мент. Поэтому вам необходимо упаковать все данные ответа в массив, который будет автоматически преобразован в объект javascript.
-		*/
-		rcmail.http_post('plugin.rm_dublecates',
-			function() {
-				$('#dstus').addClass('busy').html(rcmail.get_label('rm_duplicate_messages.checkdpl'));
-			}
-		);
-		// кнопка отмены
-		buttons[rcmail.get_label('cancel')] = function(e) {
-			$(this).remove();
-		};
+		// закрываем окно
+		$(this).remove();
+		// вызываем функцию 'msg_search'
+		msg_search(day1, month1, year1, day2, month2, year2, buttons);
+	};
+	// кнопка запуска процедуры - 'Удалить дубликаты'
+	buttons[rcmail.get_label('rm_duplicate_messages.label11')] = function(e) {
+		// закрываем окно
+		$(this).remove();
+	};
+	// кнопка отмены
+	buttons[rcmail.get_label('cancel')] = function(e) {
+		// закрываем окно
+		$(this).remove();
 	};
 	// показываем диалоговое окно
-	rcmail.show_popup_dialog(rcmail.get_label('rm_duplicate_messages.label4'), rcmail.get_label('rm_duplicate_messages.label3'), buttons);
+	rcmail.show_popup_dialog(content, title, buttons);
 }
-
 
 /**
 * Запросы и обратные вызовы Ajax
@@ -98,7 +105,7 @@ if (window.rcmail) {
 			* Пример: rcmail.register_command(команда, функция_обработчик, включить)
 			* Примечание: одна команда - один обработчик
 			*/
-			rcmail.register_command('plugin.btn_cmd_rm_dublecates', button_function, true);
+			rcmail.register_command('plugin.btn_cmd_rm_dublecates', msg_search_start, true);
 		});
 	// функция включения кнопки
 	rcmail.addEventListener('plugin.successful', function successful(){
@@ -121,3 +128,28 @@ if (window.rcmail) {
 			rcmail.refresh();
 		});
 }
+
+/**
+** Клиентские скрипты и элементы пользовательского интерфейса
+* Клиентский сценарий может фактически использовать все методы объекта приложения Roundcube.
+* Этот объект приложения, к которому можно получить доступ, window.rcmail также предоставляет перехватчики, но немного другим способом:
+*  подобно DOM браузера, можно регистрировать прослушиватели событий, используя следующие методы:
+*     rcmail.addEventListener('event', callback);
+*     rcmail.removeEventListener('event', callback);
+*
+* Самое главное событие конечно есть init. Здесь плагин может добавлять кнопки и регистрировать свои собственные команды в основном сценарии приложения.
+* Вот пример, показывающий, как может выглядеть скрипт плагина:
+*    rcmail.addEventListener('init', function(evt) {
+*       // создаем пользовательскую кнопку
+*       var button = $('<A>').attr('id', 'rcmSampleButton').html(rcmail.gettext('buttontitle', 'sampleplugin'));
+*       button.bind('click', function(e){ return rcmail.command('plugin.samplecmd', this); });
+*       // добавляем кнопку в контейнер панели инструментов и регистрируем
+*       rcmail.add_element(button, 'toolbar');
+*       // сообщим приложению, где находится кнопка и за какую команду она отвечает
+*       rcmail.register_button('plugin.samplecmd', 'rcmSampleButton', 'link');
+*       // свяжем пользовательскую команду (запускаемую обработчиком onclick кнопки) с функцией обратного вызова скрипта клиента плагина
+*       rcmail.register_command('plugin.samplecmd', sample_handler, true);
+*     });
+* // Документация по JavaScript API здесь: https://github.com/roundcube/roundcubemail/wiki/Javascript-API
+// https://github.com/roundcube/roundcubemail/wiki/Javascript-API
+*/
